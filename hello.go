d7f6464afb11dd7ff3a3aa692e0434a82da5bc18
John Chen <john.chen@arcticwolf.com>
2022-04-18 18:13:04 -0400

    Require zap logging

--- hello.go
+++ hello.go
@@ -3,14 +3,26 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	"go.uber.org/zap"
 )
 
 const msgDefault = "Hello"
 const nameDefault = "World"
 
 func main() {
+	// Initialize zap logging
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	// Default values to print.
 	msg := msgDefault
 	name := nameDefault
 
@@ -22,14 +34,17 @@ func main() {
 
 		if key == "HELLO_MSG" {
 			msg = value
+			sugar.Debugf("env HELLO_MSG=%s", msg)
 		} else if key == "HELLO_NAME" {
 			name = value
+			sugar.Debugf("env HELLO_NAME=%s", name)
 		}
 	}
 
 	// Parse command line arguments.
 	if len(os.Args) == 2 {
 		name = os.Args[1]
+		sugar.Debugf("args name=%s", name)
 	}
 
 	// Parse flags.
@@ -37,6 +52,7 @@ func main() {
 	flag.Parse()
 	if *nameFlag != "" {
 		name = *nameFlag
+		sugar.Debugf("flag name=%s", name)
 	}
 
 	// Print to stdout.
